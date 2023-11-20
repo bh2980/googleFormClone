b4d3d1dc95684f68402ad4f1efd3f84e233fdb57
bh2980 <bh2980@naver.com>
2023-11-20 23:31:30 +0900

    feat: IconButton 추가

---
+++ src/component/common/IconButton.tsx
@@ -0,0 +1,17 @@
+import classMerge from "../../utils/classMerge";
+
+interface IconButtonProps extends React.ComponentPropsWithRef<"button"> {}
+
+const IconButton = ({ children, className, type = "button", ...props }: IconButtonProps) => {
+  return (
+    <button
+      className={classMerge(["p-4", "rounded-full", "hover:bg-gray-200", "active:bg-gray-300", className])}
+      type={type}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default IconButton;
---
+++ src/constants.ts
@@ -0,0 +1,1 @@
+export const ICON_CLASS = "w-[20px] h-[20px] text-gray-600";
