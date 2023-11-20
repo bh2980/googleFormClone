f3123d11c0e3db0b196ae0c4a23887722d572bb9
bh2980 <bh2980@naver.com>
2023-11-20 22:57:25 +0900

    design: Input 컴포넌트 패딩 추가

--- src/component/common/Input.tsx
+++ src/component/common/Input.tsx
@@ -3,7 +3,7 @@ import classMerge from "../../utils/classMerge";
 interface InputProps extends React.ComponentPropsWithRef<"input"> {}
 
 const Input = ({ className, ...props }: InputProps) => {
-  return <input className={classMerge(["focus:border-b-2 border-violet-800", className])} {...props} />;
+  return <input className={classMerge(["focus:border-b-2 border-violet-800 py-2", className])} {...props} />;
 };
 
 export default Input;
