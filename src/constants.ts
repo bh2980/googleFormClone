a0ae23ba7c255d5b78ce0a2e960fb486dcbaac04
bh2980 <bh2980@naver.com>
2023-11-21 05:27:42 +0900

    chore: 드롭다운 종류 상수화

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,9 @@
 export const ICON_CLASS = "w-[20px] h-[20px] text-gray-600";
+
+export const EDITOR_DROPDOWN_LIST = [
+  { content: "단답형", type: "short" as const },
+  { content: "장문형", type: "long" as const },
+  { content: "객관식 질문", type: "radio" as const },
+  { content: "체크박스", type: "checkbox" as const },
+  { content: "드롭박스", type: "dropdown" as const },
+];
