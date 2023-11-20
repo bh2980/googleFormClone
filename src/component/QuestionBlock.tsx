ba752164068579578da50fbb1162720b81690c8b
bh2980 <bh2980@naver.com>
2023-11-21 08:48:07 +0900

    feat: 질문 속성 수정 및 삭제 기능 추가

--- src/component/QuestionBlock.tsx
+++ src/component/QuestionBlock.tsx
@@ -6,25 +6,54 @@ import { EDITOR_DROPDOWN_LIST, ICON_CLASS } from "../constants";
 import IconButton from "./common/IconButton";
 import Switch from "./common/Switch";
 import Dropdown from "./common/Dropdown";
-import { useState } from "react";
+import { useEffect } from "react";
 
-import type { QuestionKindType } from "../store/questionSlice";
+import { removeQuestion, type QuestionKindType, editQuestion, copyQuestion } from "../store/questionSlice";
+import { useAppDispatch, useAppSelector } from "../hook/storeHook";
 
 interface QuestionBlockProps extends React.ComponentPropsWithRef<"div"> {
+  questionID: string;
   type?: QuestionKindType;
   isForm?: boolean;
 }
 /**
  * QuestionBlock은 BlockID를 받아서 QuestionManager에게 전달
  */
-const QuestionBlock = ({ type = "short", ...props }: QuestionBlockProps) => {
-  const [blockType, setBlockType] = useState<QuestionKindType>(type);
+const QuestionBlock = ({ questionID, type = "short", ...props }: QuestionBlockProps) => {
+  const dispatch = useAppDispatch();
+  const questionInfo = useAppSelector((store) => store.question[questionID]);
+  const { questionContent, required } = questionInfo;
+  const answerMap = useAppSelector((store) => store.answer);
 
-  const changeBlockType = (idx: number) => {
-    //blockType이 변경되면 block의 questionList 초기화
-    console.log(idx, EDITOR_DROPDOWN_LIST[idx]);
-    setBlockType(EDITOR_DROPDOWN_LIST[idx].type);
+  const removeQuestionBlock = () => {
+    dispatch(removeQuestion(questionInfo));
   };
+
+  const copyQuestionBlock = () => {
+    // answerList로부터 answer를 가져와 deepcopy -> answerMap에 새로 등록
+    // 새 uuid 발급
+    // 그 외 정보 그대로 복사해서 copyQuestion
+    // REDUX에서는 copyQuestion시 questionMap에 신규 등록
+    // docsSlice에서 splice를 통해 이전 questionID 이후에 삽입
+    // dispatch(copyQuestion(questionInfo));
+  };
+
+  const changeQuestionType = (idx: number) => {
+    //blockType이 변경되면 block의 questionList 초기화
+    dispatch(editQuestion({ ...questionInfo, type: EDITOR_DROPDOWN_LIST[idx].type }));
+  };
+
+  const changeQuestionContent = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(editQuestion({ ...questionInfo, questionContent: e.target.value }));
+  };
+
+  const changeRequired = () => {
+    dispatch(editQuestion({ ...questionInfo, required: !required }));
+  };
+
+  useEffect(() => {
+    console.log("정보 변경", questionInfo);
+  }, [questionInfo]);
 
   // 나중에
   const dragBlock = () => {
@@ -38,25 +67,30 @@ const QuestionBlock = ({ type = "short", ...props }: QuestionBlockProps) => {
       </div>
       <div className="flex flex-col gap-6 px-6 pb-6">
         <div className="flex items-center justify-between gap-4 group">
-          <Input placeholder="질문" className="focus:hover:bg-gray-200 focus:bg-gray-100 focus:p-4 w-[440px]" />
+          <Input
+            placeholder="질문"
+            className="focus:hover:bg-gray-200 focus:bg-gray-100 focus:p-4 w-[440px]"
+            onChange={changeQuestionContent}
+            value={questionContent}
+          />
           <Dropdown
             className="hidden group-focus-within:flex"
             itemList={EDITOR_DROPDOWN_LIST}
-            onChange={changeBlockType}
+            onChange={changeQuestionType}
           />
         </div>
         {/** 여기를 어떻게 처리한담 */}
         <div className="flex-col hidden group-focus-within:flex group-focus-within:flex-col group-focus-within:gap-4">
           <Divider />
           <div className="flex items-center justify-end gap-2 pt-4 h-[48px]">
-            <IconButton>
+            <IconButton onClick={copyQuestionBlock}>
               <RiFileCopyLine className={ICON_CLASS} />
             </IconButton>
-            <IconButton>
+            <IconButton onClick={removeQuestionBlock}>
               <RiDeleteBin6Line className={ICON_CLASS} />
             </IconButton>
             <Divider direction="vertical" className="mx-2" />
-            <Switch description="필수" descriptionPos="before" />
+            <Switch description="필수" descriptionPos="before" checked={required} onChange={changeRequired} />
           </div>
         </div>
       </div>
