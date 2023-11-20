4f81673b5a5fcb76ade5671961c0cef699552341
bh2980 <bh2980@naver.com>
2023-11-21 01:48:29 +0900

    feat: Switch 추가

--- src/component/QuestionBlock.tsx
+++ src/component/QuestionBlock.tsx
@@ -12,19 +12,18 @@ import Divider from "./common/Divider";
 import { ICON_CLASS } from "../constants";
 import classMerge from "../utils/classMerge";
 import IconButton from "./common/IconButton";
+import Switch from "./common/Switch";
 
 interface QuestionBlockProps extends React.ComponentPropsWithRef<"div"> {
   type?: "short" | "long" | "radio" | "checkbox" | "dropdown";
 }
 
 const QuestionBlock = ({ type = "short", ...props }: QuestionBlockProps) => {
-  console.log(type);
-
   return (
     <Block className="w-full" {...props}>
-      <div className="flex justify-center py-2">
-        <RiDraggable className="rotate-90 cursor-move" />
-      </div>
+      <button className="flex justify-center w-full py-2 cursor-move">
+        <RiDraggable className="rotate-90" />
+      </button>
       <div className="flex flex-col gap-6 px-6 pb-6">
         <div className="flex items-center justify-between gap-4 group">
           <Input placeholder="질문" className="focus:hover:bg-gray-200 focus:bg-gray-100 focus:p-4 w-[380px]" />
@@ -36,19 +35,18 @@ const QuestionBlock = ({ type = "short", ...props }: QuestionBlockProps) => {
             드롭박스
           </div>
         </div>
-        <input placeholder="단답형 메시지" readOnly />
-        <div className="hidden group-focus-within:flex group-focus-within:flex-col group-focus-within:gap-4">
+        <input placeholder="단답형 메시지" readOnly /> {/** 여기게 type 렌더링 */}
+        <div className="flex-col hidden group-focus-within:flex group-focus-within:flex-col group-focus-within:gap-4">
           <Divider />
-          <div className="flex items-center justify-end gap-4 h-[32px]">
+          <div className="flex items-center justify-end gap-2 pt-4 h-[48px]">
             <IconButton>
               <RiFileCopyLine className={ICON_CLASS} />
             </IconButton>
             <IconButton>
               <RiDeleteBin6Line className={ICON_CLASS} />
             </IconButton>
-            <Divider direction="vertical" />
-            <span>필수</span>
-            <div>스위치</div>
+            <Divider direction="vertical" className="mx-2" />
+            <Switch description="필수" descriptionPos="before" />
             <IconButton>
               <RiMore2Fill className={ICON_CLASS} />
             </IconButton>
