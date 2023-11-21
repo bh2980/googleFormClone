b5f5e09c0c540e7bb2f090dbd2a347d8e35eceea
bh2980 <bh2980@naver.com>
2023-11-21 10:27:18 +0900

    feat: AnswerManager 추가

---
+++ src/component/AnswerItemManager.tsx
@@ -0,0 +1,85 @@
+/** AnswerManager는 질문 부분을 관리하는 매니저
+ * questionID를 이용해 type과 answerIDLIst를 가져온다
+ * answer 상태에서 answerIDLIst를 순회하면서 answer을 뽑아 알맞게 렌더링한다
+ */
+
+import { useAppDispatch, useAppSelector } from "../hook/storeHook";
+import { AnswerInterface, addAnswer, editAnswer, removeAnswer } from "../store/answerSlice";
+import ChooseAnswer from "./Question/ChooseAnswer";
+import LongAnswer from "./Question/LongAnswer";
+import ShortAnswer from "./Question/ShortAnswer";
+import { v4 as uuidv4 } from "uuid";
+
+interface AnswerManagerProps {
+  isForm: boolean;
+  questionID: string;
+}
+
+const AnswerManager = ({ isForm, questionID }: AnswerManagerProps) => {
+  const dispatch = useAppDispatch();
+  const { type, answerIDList } = useAppSelector((store) => store.question[questionID]);
+  const answerMap = useAppSelector((store) => store.answer);
+
+  const changeAnswer = (e: React.ChangeEvent<HTMLInputElement>, { answerID, questionID }: AnswerInterface) => {
+    dispatch(editAnswer({ answerID, content: e.target.value, questionID }));
+  };
+
+  const addAnswerItem = () => {
+    //옵션 추가 시 새 answer에 focus가 위치하도록 수정
+    const NEW_ANSWER_ID = uuidv4();
+    dispatch(addAnswer({ answerID: NEW_ANSWER_ID, content: "", questionID }));
+  };
+
+  const removeAnswerItem = (aID: string) => {
+    //remove시 마지막 answer에 focus가 위차하도록 수정
+    dispatch(removeAnswer(answerMap[aID]));
+  };
+
+  return (
+    <>
+      {type === "short" ? (
+        <ShortAnswer isForm={isForm} />
+      ) : type === "long" ? (
+        <LongAnswer isForm={isForm} />
+      ) : type === "radio" || type === "checkbox" || type === "dropdown" ? (
+        <>
+          {answerIDList.map((aID, idx) => {
+            const answerInfo = answerMap[aID];
+            return (
+              <ChooseAnswer
+                key={aID}
+                idx={idx + 1}
+                inputType={type}
+                value={answerInfo.content}
+                placeholder={`옵션 ${idx + 1}`}
+                onChange={(e) => changeAnswer(e, answerInfo)}
+                deletable={answerIDList.length > 1}
+                onDeleteButton={() => removeAnswerItem(aID)}
+              />
+            );
+          })}
+          <div className="flex items-center gap-4">
+            <div className="flex items-center w-[104px] gap-4">
+              <div className="flex justify-center w-[16px]">
+                {type !== "dropdown" && <input type={type} disabled />}
+              </div>
+              <span
+                className="w-full text-gray-500 cursor-text hover:underline decoration-gray-400"
+                onClick={addAnswerItem}
+                tabIndex={0}
+              >
+                옵션 추가
+              </span>
+            </div>
+            {/* <span>또는</span>
+            <button type="button" className="text-blue-500">
+              '기타' 추가
+            </button> */}
+          </div>
+        </>
+      ) : null}
+    </>
+  );
+};
+
+export default AnswerManager;
--- src/component/QuestionBlock.tsx
+++ src/component/QuestionBlock.tsx
@@ -10,6 +10,7 @@ import { useEffect } from "react";
 
 import { removeQuestion, type QuestionKindType, editQuestion, copyQuestion } from "../store/questionSlice";
 import { useAppDispatch, useAppSelector } from "../hook/storeHook";
+import AnswerManager from "./AnswerItemManager";
 
 interface QuestionBlockProps extends React.ComponentPropsWithRef<"div"> {
   questionID: string;
@@ -39,7 +40,9 @@ const QuestionBlock = ({ questionID, type = "short", ...props }: QuestionBlockPr
   };
 
   const changeQuestionType = (idx: number) => {
-    //blockType이 변경되면 block의 questionList 초기화
+    //questionType이 변경되면 question의 answerList 초기화
+    //answerMap에서 answerList 항목 제거 delete
+    //answerList를 빈 배열로 변경
     dispatch(editQuestion({ ...questionInfo, type: EDITOR_DROPDOWN_LIST[idx].type }));
   };
 
@@ -61,7 +64,7 @@ const QuestionBlock = ({ questionID, type = "short", ...props }: QuestionBlockPr
   };
 
   return (
-    <Block className="w-full" {...props}>
+    <Block className="w-full group" {...props}>
       <div className="flex justify-center w-full py-2 cursor-move group" onClick={dragBlock}>
         <RiDraggable className="invisible rotate-90 group-hover:visible" />
       </div>
@@ -69,7 +72,7 @@ const QuestionBlock = ({ questionID, type = "short", ...props }: QuestionBlockPr
         <div className="flex items-center justify-between gap-4 group">
           <Input
             placeholder="질문"
-            className="focus:hover:bg-gray-200 focus:bg-gray-100 focus:p-4 w-[440px]"
+            className="group-focus-within:border-gray-300 group-focus-within:border-b-2 group-focus-within:hover:bg-gray-200 group-focus-within:bg-gray-100 group-focus-within:p-4 w-[440px]"
             onChange={changeQuestionContent}
             value={questionContent}
           />
@@ -79,7 +82,7 @@ const QuestionBlock = ({ questionID, type = "short", ...props }: QuestionBlockPr
             onChange={changeQuestionType}
           />
         </div>
-        {/** 여기를 어떻게 처리한담 */}
+        <AnswerManager questionID={questionID} isForm={false} />
         <div className="flex-col hidden group-focus-within:flex group-focus-within:flex-col group-focus-within:gap-4">
           <Divider />
           <div className="flex items-center justify-end gap-2 pt-4 h-[48px]">
