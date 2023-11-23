c9fd804f4372b8f761356c6a2701ebb163821866
bh2980 <bh2980@naver.com>
2023-11-24 00:09:32 +0900

    fix: disabled 동작 추가

--- src/component/common/Checkbox.tsx
+++ src/component/common/Checkbox.tsx
@@ -4,10 +4,10 @@ interface CheckboxProps extends React.ComponentPropsWithoutRef<"input"> {
 
 const Checkbox = ({ label, ...props }: CheckboxProps) => {
   return (
-    <label className="flex items-center gap-4">
+    <label className="flex items-center gap-4 group">
       <input type="checkbox" className="hidden peer" {...props} />
-      <div className="w-[20px] h-[20px] rounded-lg border-2 border-gray-500 peer-checked:bg-violet-500" />
-      <span>{label}</span>
+      <div className="relative flex justify-center items-center w-[20px] h-[20px] rounded-lg border-2 border-gray-600 peer-checked:bg-violet-500 group-hover:before:absolute group-hover:before:w-[40px] group-hover:before:h-[40px] group-hover:before:bg-violet-300/50 group-hover:before:rounded-xl peer-disabled:before:hidden peer-disabled:opacity-50" />
+      {label && <span>{label}</span>}
     </label>
   );
 };
--- src/component/common/Radio.tsx
+++ src/component/common/Radio.tsx
@@ -1,25 +1,26 @@
 import { useState } from "react";
 
-interface RadioProps extends Omit<React.ComponentPropsWithoutRef<"input">, "onChange"> {
-  onChange?: (event: React.MouseEvent) => void;
+interface RadioProps extends React.ComponentPropsWithoutRef<"input"> {
   label?: string;
 }
 
-const Radio = ({ label, checked = false, onChange, ...props }: RadioProps) => {
+// 선택 해제 가능한 Radio
+const Radio = ({ label, checked = false, ...props }: RadioProps) => {
   const [radioChecked, setRadioChecked] = useState(checked);
 
   const changeRadioChecked = (e: React.MouseEvent) => {
     e.preventDefault();
 
-    if (onChange) onChange(e);
+    if (props.disabled) return;
+
     setRadioChecked((prev) => !prev);
   };
 
   return (
     <label className="flex items-center gap-4 group" onClick={changeRadioChecked}>
       <input type="radio" className="hidden peer" {...props} checked={radioChecked} />
-      <div className="border-2 border-gray-600 relative flex justify-center items-center w-[20px] h-[20px] rounded-full before:absolute before:flex before:items-center before:w-[10px] before:h-[10px] before:rounded-full peer-checked:before:bg-violet-500 group-hover:after:w-[40px] group-hover:after:h-[40px] group-hover:after:absolute group-hover:after:bg-violet-300/50 group-hover:after:rounded-full" />
-      <span>{label}</span>
+      <div className="border-2 border-gray-600 relative flex justify-center items-center w-[20px] h-[20px] rounded-full before:absolute before:flex before:items-center before:w-[10px] before:h-[10px] before:rounded-full peer-checked:before:bg-violet-500 group-hover:after:w-[40px] group-hover:after:h-[40px] group-hover:after:absolute group-hover:after:bg-violet-300/50 group-hover:after:rounded-full peer-disabled:after:hidden peer-disabled:opacity-50" />
+      {label && <span>{label}</span>}
     </label>
   );
 };
