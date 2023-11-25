8cb4226ac0d1974e21beb1aa3c21d4e870c1dfe8
bh2980 <bh2980@naver.com>
2023-11-25 20:19:43 +0900

    chore: console.log 삭제제

--- src/hook/useChangeEditBlockID.ts
+++ src/hook/useChangeEditBlockID.ts
@@ -6,8 +6,6 @@ const useChangeEditBlockID = (blockID: string) => {
   const edtingBlockID = useAppSelector((store) => store.docs.editBlockID);
   const isEditing = edtingBlockID === blockID;
 
-  console.log("hook", blockID, edtingBlockID);
-
   const changeEditingBlockID = () => dispatch(changeEditBlockIDAction(blockID));
 
   return { edtingBlockID, changeEditingBlockID, isEditing };
