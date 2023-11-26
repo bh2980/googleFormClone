a974d2b83b1d1faabb6370f97e2a6974d6c7ac09
bh2980 <bh2980@naver.com>
2023-11-27 05:08:38 +0900

    feat: form용 TitleBlock 추가

---
+++ src/component/form/TitleBlock.tsx
@@ -0,0 +1,16 @@
+import { useAppSelector } from "../../hook/storeHook";
+import Block from "../common/Block";
+import TextArea from "../common/TextArea";
+
+const TitleBlock = () => {
+  const { title, content } = useAppSelector((store) => store.docs);
+
+  return (
+    <Block className="flex flex-col w-full gap-2 p-6" isTitleBlock>
+      <span className="text-3xl">{title}</span>
+      <TextArea>{content}</TextArea>
+    </Block>
+  );
+};
+
+export default TitleBlock;
