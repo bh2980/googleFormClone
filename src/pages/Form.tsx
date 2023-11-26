614e2a81fa45f65749e9fef66fc2c2fa27ed5ab7
bh2980 <bh2980@naver.com>
2023-11-26 20:57:01 +0900

    feat: form->editor 이동 버튼 추가

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -1,5 +1,25 @@
+import { RiEdit2Line } from "react-icons/ri";
+import IconButton from "../component/common/IconButton";
+
+import { Link } from "react-router-dom";
+
 const Form = () => {
-  return <div>Form</div>;
+  return (
+    <div className="flex flex-col min-h-screen bg-violet-100">
+      <div className="w-full h-[56px] flex justify-end shadow-sm border-b-gray-200 border-b-[1px] p-4 items-center bg-gray-50">
+        <IconButton>
+          <Link to={"/editor"}>
+            <RiEdit2Line className="w-[20px] h-[20px] text-gray-600" />
+          </Link>
+        </IconButton>
+      </div>
+      <div className="flex w-full p-8">
+        <div className="flex-1"></div>
+        <form className="flex flex-[2] gap-4 flex-col "></form>
+        <div className="flex-1"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Form;
