eebddd1f69650ae3453b7b25d2e70ee27891fcc0
bh2980 <bh2980@naver.com>
2023-11-27 16:34:47 +0900

    chore: value props -> label 이름 변경

--- src/component/preview/ChooseAnswer.tsx
+++ src/component/preview/ChooseAnswer.tsx
@@ -3,19 +3,19 @@ import Checkbox from "../common/Checkbox";
 import Radio from "../common/Radio";
 
 interface RadioAnswerProps extends Omit<React.ComponentPropsWithRef<"input">, "type"> {
-  type: EDITOR_QUESTION_TYPE.radio | EDITOR_QUESTION_TYPE.checkbox | EDITOR_QUESTION_TYPE.dropdown;
-  value?: string;
+  type: EDITOR_QUESTION_TYPE.radio | EDITOR_QUESTION_TYPE.checkbox;
+  label?: string;
 }
 
-const ChooseAnswer = ({ type, value, ...props }: RadioAnswerProps) => {
+const ChooseAnswer = ({ type, label, ...props }: RadioAnswerProps) => {
   return (
     <div className="flex items-center justify-between">
       <div className="relative flex items-center w-full gap-4">
         <div className="flex gap-4 px-[32px] py-1 w-full items-center">
           {type === EDITOR_QUESTION_TYPE.radio ? (
-            <Radio label={value} {...props} />
+            <Radio label={label} {...props} />
           ) : (
-            <Checkbox label={value} {...props} />
+            <Checkbox label={label} {...props} />
           )}
         </div>
       </div>
