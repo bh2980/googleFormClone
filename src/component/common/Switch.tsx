b9c68c02d17ba2374b238036c7b84c5fd6bee70e
bh2980 <bh2980@naver.com>
2023-11-29 02:13:39 +0900

    design: switch 이동 애니메이션 추가

--- src/component/common/Switch.tsx
+++ src/component/common/Switch.tsx
@@ -1,3 +1,5 @@
+import classMerge from "../../utils/classMerge";
+
 interface SwitchProps extends React.ComponentPropsWithRef<"input"> {
   description?: string;
   descriptionPos?: "before" | "after";
@@ -10,7 +12,23 @@ const Switch = ({ description, descriptionPos = "after", ...props }: SwitchProps
       <div className="relative flex items-center cursor-pointer w-[48px] h-[24px]">
         <input className="hidden peer" type="checkbox" {...props} />
         <div className="absolute h-[20px] w-[48px] bg-gray-300 peer-checked:bg-violet-100 rounded-full"></div>
-        <span className="absolute h-[24px] w-[24px] bg-gray-100 shadow-md rounded-full peer-checked:bg-violet-500 peer-checked:translate-x-[100%] hover:ring-8 hover:ring-gray-300/50 peer-checked:hover:ring-violet-400/50" />
+        <span
+          className={classMerge([
+            "absolute",
+            "h-[24px]",
+            "w-[24px]",
+            "bg-gray-100",
+            "shadow-md",
+            "rounded-full",
+            "hover:ring-8",
+            "transition-all",
+            "duration-300",
+            "hover:ring-gray-300/50",
+            "peer-checked:bg-violet-500",
+            "peer-checked:translate-x-[100%]",
+            "peer-checked:hover:ring-violet-400/50",
+          ])}
+        />
       </div>
       {descriptionPos === "after" && <span className="cursor-pointer">{description}</span>}
     </label>
