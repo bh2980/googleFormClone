cf8410bdeb54b57936ec4233e198235e6b320a28
bh2980 <bh2980@naver.com>
2023-11-28 19:45:47 +0900

    fix: editor에서 시작 시 설명 높이 축소 문제 fix

--- src/component/common/TextArea.tsx
+++ src/component/common/TextArea.tsx
@@ -1,17 +1,26 @@
+import { useEffect, useRef } from "react";
 import classMerge from "../../utils/classMerge";
 
 interface TextAreaProps extends React.ComponentPropsWithoutRef<"textarea"> {}
 
 //TODO value에 따라 첫 높이 변경
 const TextArea = ({ className, ...props }: TextAreaProps) => {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
   //ref를 활용하는 방법도 존재
   const handleInput = (e: React.FormEvent<HTMLTextAreaElement>) => {
     const target = e.target as HTMLTextAreaElement;
 
-    target.style.height = "inherit";
+    target.style.height = "inherit"; // 높이 감소 시 필요
     //스크롤의 길이만큼 실시간으로 높이를 늘려서 스크롤을 없앰
     target.style.height = `${target.scrollHeight}px`;
   };
+
+  useEffect(() => {
+    // 시작 시 높이 지정
+    if (!textareaRef?.current) return;
+
+    textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+  }, []);
 
   return (
     <textarea
@@ -21,6 +30,7 @@ const TextArea = ({ className, ...props }: TextAreaProps) => {
       ])}
       rows={1}
       onInput={handleInput}
+      ref={textareaRef}
       {...props}
     />
   );
