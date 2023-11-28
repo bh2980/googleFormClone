105b4bcb4e52c67ca5255799d53d3e2ee62249da
bh2980 <bh2980@naver.com>
2023-11-28 19:45:20 +0900

    feat: preview/TitleBlock에서 content를 div에 들어가도록 수정

--- src/component/preview/TitleBlock.tsx
+++ src/component/preview/TitleBlock.tsx
@@ -1,6 +1,5 @@
 import { useAppSelector } from "../../hook/storeHook";
 import Block from "../common/Block";
-import TextArea from "../common/TextArea";
 
 const TitleBlock = () => {
   const { title, content } = useAppSelector((store) => store.docs);
@@ -8,7 +7,14 @@ const TitleBlock = () => {
   return (
     <Block className="flex flex-col w-full gap-2 p-6" isTitleBlock>
       <span className="text-3xl">{title}</span>
-      <TextArea disabled>{content}</TextArea>
+      <div>
+        {content.split("\n").map((line) => (
+          <>
+            <span>{line}</span>
+            <br />
+          </>
+        ))}
+      </div>
     </Block>
   );
 };
