24eeff54634d66c1ca2e4ba5ace91c78ab954cbe
bh2980 <bh2980@naver.com>
2023-12-09 01:28:19 +0900

    feat: 리스트 아이템 왕복 로직 완료

--- src/hook/headless/useDnDList_unstable.tsx
+++ src/hook/headless/useDnDList_unstable.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useMemo, useRef } from "react";
 
 interface useDnDProps {
   handleItem: (fromIdx: number, toIdx: number) => void;
@@ -22,6 +22,24 @@ const makeTranslate = (x: number, y: number) => `translate(${makePx(x)}, ${makeP
 const useDnDList_unstable = <T extends HTMLElement = HTMLDivElement>({ handleItem }: useDnDProps) => {
   const containerRef = useRef<T>(null);
   const stackRef = useRef<number[]>([]);
+  const prevDiff = useRef(0);
+
+  const isReverse = (diff: number, stackTop: number, belowItemIdx: number) => {
+    if (stackTop === belowItemIdx) {
+      prevDiff.current = prevDiff.current * -1;
+      return true;
+    }
+
+    if (prevDiff.current === 0) {
+      prevDiff.current = diff;
+      return false;
+    }
+
+    if (prevDiff.current !== diff) {
+      prevDiff.current = diff;
+      return true;
+    } else return false;
+  };
 
   const getDragIdx = (item: HTMLElement) => Number(item.dataset.dragIdx);
   const getStackTop = () => stackRef.current[stackRef.current.length - 1];
@@ -30,6 +48,9 @@ const useDnDList_unstable = <T extends HTMLElement = HTMLDivElement>({ handleIte
     if (!containerRef?.current) return;
 
     e.preventDefault();
+
+    // TODO 추후 수정 -> useMemo를 통한 성능 최적화
+    // const itemList = [...containerRef.current.childNodes] as HTMLElement[];
 
     const dragStartPoint = { clientX: e.clientX, clientY: e.clientY };
 
@@ -59,7 +80,7 @@ const useDnDList_unstable = <T extends HTMLElement = HTMLDivElement>({ handleIte
       transition: "",
     });
 
-    setStyle(placeholder, { opacity: "0.5", border: "1px solid blue" });
+    setStyle(placeholder, { opacity: "0.5", border: "1px solid blue", pointerEvents: "none" });
 
     containerRef.current.appendChild(moveItem);
 
@@ -84,30 +105,66 @@ const useDnDList_unstable = <T extends HTMLElement = HTMLDivElement>({ handleIte
         const belowItemIdx = getDragIdx(belowElement);
         const stackTop = getStackTop();
 
-        if (belowItemIdx === stackTop || belowItemIdx === dragItemIdx) return; // stack에 이미 들어갔을 경우 처리 X
+        if (belowItemIdx === dragItemIdx) return;
 
-        console.log(belowItemIdx);
+        // console.log(belowItemIdx);
         // stacktop부터 현재 드래그까지 모든 idx에 대해 처리
         const diff = belowItemIdx > stackTop ? 1 : -1;
 
-        if (isMoved(belowElement)) {
-        } else {
-          const processIdxArr = Array.from(
-            { length: (belowItemIdx - stackTop) / diff },
-            (_, i) => stackTop + (i + 1) * diff
-          ).forEach((itemIdx) => {
-            const movingElement = containerRef.current?.childNodes[itemIdx] as HTMLElement;
-            const movingDistance = MOVE_DISTANCE * diff * -1;
+        const tempIsReverse = isReverse(diff, stackTop, belowItemIdx);
 
-            const ghostDistance = MOVE_DISTANCE * (itemIdx - dragItemIdx);
+        const progressIdx = tempIsReverse
+          ? Array.from({ length: Math.abs(belowItemIdx - stackTop) + 1 }, (_, i) => stackTop + i * diff)
+          : Array.from({ length: Math.abs(belowItemIdx - stackTop) }, (_, i) => stackTop + (i + 1) * diff);
 
-            setStyle(placeholder, { transform: makeTranslate(0, ghostDistance) });
+        console.log("progressIdx :", progressIdx, "stackTop :", stackTop);
+        console.log("diff", diff, "prevDiff", prevDiff.current, "isReverse :", tempIsReverse);
 
-            setStyle(movingElement, { transform: makeTranslate(0, movingDistance) });
-            movingElement.classList.add("moved");
-          });
-          console.log(processIdxArr);
-        }
+        progressIdx.forEach((itemIdx) => {
+          if (itemIdx === dragItemIdx) return;
+
+          const targetItem = containerRef.current?.childNodes[itemIdx] as HTMLElement;
+
+          if (isMoved(targetItem)) {
+            // 왕복 로직 -> 한 번 이동된 아이들을 다시 처리
+            const backItemIdx = stackRef.current.pop();
+            // 한 번 이동된 아이들은 moved 클래스가 추가된다.
+            const placeholderMove = (backItemIdx! - dragItemIdx + diff) * MOVE_DISTANCE;
+
+            setStyle(placeholder, { transform: makeTranslate(0, placeholderMove) });
+            setStyle(targetItem, { transform: makeTranslate(0, 0), pointerEvents: "none" });
+            targetItem.classList.remove("moved");
+
+            targetItem.addEventListener(
+              "transitionend",
+              () => {
+                setStyle(targetItem, { pointerEvents: "" });
+              },
+              { once: true }
+            );
+
+            // moved 클래스를 빼고 translate이 적용되어있던 것을 제거하면 원래 위치로 돌아온다.
+            // 기존 것들도 반복하면서 로직이 꼬임
+          } else {
+            // 편도 로직
+            const placeholderMove = (belowItemIdx - dragItemIdx) * MOVE_DISTANCE;
+            const targetMove = MOVE_DISTANCE * diff * -1;
+
+            setStyle(placeholder, { transform: makeTranslate(0, placeholderMove) });
+            setStyle(targetItem, { transform: makeTranslate(0, targetMove), pointerEvents: "none" });
+            targetItem.classList.add("moved");
+
+            targetItem.addEventListener(
+              "transitionend",
+              () => {
+                setStyle(targetItem, { pointerEvents: "" });
+              },
+              { once: true }
+            );
+
+            stackRef.current.push(itemIdx);
+          }
+        });
 
         // 드래그 idx면 제거
         // stacktop과 idx가 같으면 빼고 translate 적용 제거
@@ -119,11 +176,26 @@ const useDnDList_unstable = <T extends HTMLElement = HTMLDivElement>({ handleIte
       document.removeEventListener("mousemove", mouseMove);
       moveItem.remove();
 
-      placeholder.removeAttribute("style");
+      placeholder.removeAttribute("style"); // style을 다 제거하면 안됨
+      placeholder.removeAttribute("data-drag-idx");
+
+      containerRef.current?.childNodes.forEach((item) => {
+        const listItem = item as HTMLElement;
+        listItem.removeAttribute("style"); // style을 다 제거하면 안됨
+        listItem.classList.remove("moved");
+        listItem.classList.remove("moving");
+      });
+
+      console.log(stackRef.current, dragItemIdx, getStackTop());
+
+      handleItem(dragItemIdx, getStackTop());
+
+      stackRef.current = [];
+      prevDiff.current = 0;
     };
 
     document.addEventListener("mousemove", mouseMove);
-    document.addEventListener("mouseup", mouseUp);
+    document.addEventListener("mouseup", mouseUp, { once: true });
   };
 
   return { containerRef, dragStart };
