71ba4b92a2337f569f79d7200a4a9d9d878041e0
bh2980 <bh2980@naver.com>
2023-12-11 23:04:56 +0900

    feat: clone 및 move 로직 추가

--- src/hook/headless/useDnDList_unstable.tsx
+++ src/hook/headless/useDnDList_unstable.tsx
@@ -13,14 +13,20 @@ const setStyle = (target: HTMLElement, style: Partial<CSSStyleDeclaration>) => {
   Object.assign(target.style, style);
 };
 
+const makePx = (px: number) => `${px}px`;
+
+const makeTransition = (x: number, y: number) => `translate3d(${makePx(x)}, ${makePx(y)}, 0)`;
+
 const useDnDList_unstable = <T extends HTMLElement = HTMLDivElement>({ handleItem }: useDnDProps) => {
   const indexRef = useRef(-1);
   const constainerRef = useRef<T>(null);
 
-  const onMouseDown = (e: React.MouseEvent<Element, MouseEvent>) => {
+  const mouseDownHandler = (e: React.MouseEvent<Element, MouseEvent>) => {
     e.preventDefault();
 
     if (!constainerRef.current) return;
+
+    const { clientX: dragStartX, clientY: dragStartY } = e;
 
     const itemList = [...constainerRef.current.childNodes] as HTMLElement[];
     const aboveItemList: HTMLElement[] = [];
@@ -48,20 +54,52 @@ const useDnDList_unstable = <T extends HTMLElement = HTMLDivElement>({ handleIte
       }
     });
 
-    const dragItem = itemList[dragIdx];
+    const placeholder = itemList[dragIdx];
 
-    console.log(aboveItemList, aboveItemSet);
-    console.log(belowItemList, belowItemSet);
-    console.log(dragItem, dragIdx);
+    //cloneItem for drag
 
-    const mouseMoveHandler = (moveEvent: MouseEvent) => {};
+    const handleItem = placeholder.cloneNode(true) as HTMLElement;
+
+    const { top, left, width, height } = placeholder.getBoundingClientRect();
+
+    setStyle(handleItem, {
+      position: "fixed",
+      top: makePx(top),
+      left: makePx(left),
+      width: makePx(width),
+      height: makePx(height),
+      pointerEvents: "none",
+      zIndex: "9999",
+    });
+
+    setStyle(placeholder, {
+      opacity: "0.5",
+    });
+
+    constainerRef.current.appendChild(handleItem);
+
+    const mouseMoveHandler = (moveEvent: MouseEvent) => {
+      moveEvent.preventDefault();
+
+      const { clientX, clientY } = moveEvent;
+
+      const deltaX = clientX - dragStartX;
+      const deltaY = clientY - dragStartY;
+
+      setStyle(handleItem, { transform: makeTransition(deltaX, deltaY) });
+    };
+
+    const mouseUpHandler = () => {
+      document.removeEventListener("mousemove", mouseMoveHandler);
+      handleItem.remove();
+    };
 
     document.addEventListener("mousemove", mouseMoveHandler);
-    document.addEventListener("mouseup", () => {}, { once: true });
+    document.addEventListener("mouseup", mouseUpHandler, { once: true });
   };
 
   return {
-    handleDrag: onMouseDown,
+    handleDrag: mouseDownHandler,
     constainerRef,
   };
 };
