849400663ab6e45bf7f21773b270b1d934a5ca5c
luke-lemon <luke@lemoncloud.io>
2024-06-11 16:27:28 +0900

    chore: hooks folder setup

---
+++ library/hooks/src/lib/hooks.tsx
@@ -0,0 +1,18 @@
+import styled from 'tailwind';
+
+/* eslint-disable-next-line */
+export interface HooksProps {}
+
+const StyledHooks = styled.div`
+  color: pink;
+`;
+
+export function Hooks(props: HooksProps) {
+  return (
+    <StyledHooks>
+      <h1>Welcome to Hooks!</h1>
+    </StyledHooks>
+  );
+}
+
+export default Hooks;
